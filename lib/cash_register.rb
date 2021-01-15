04cfa5e3acfb5a43a2cb7124d4d84dbcd19db5b1
Jason Herrera <baytownjason@yahoo.com>
2021-01-15 08:58:58 -0600

    commit tree cc0bf1368df971ac190c1c4e044939b0181484ef

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require 'pry'
+class CashRegister
+    attr_accessor :total, :discount
+    attr_reader 
+  
+    def initialize(discount=0)
+        @discount = discount
+        @total = total = 0
+        @cart = []
+    end
+    def total
+        @total
+    end
+    def add_item(title, price, quantity=1)
+        @total += price * quantity
+        quantity.times { @cart << title }
+        @total
+    end
+    def apply_discount(discount=@discount)
+        #discount = @discount
+        if discount > 0
+            discount = discount.to_f
+            savings = (discount * @total / 100).round(2)
+            savings = '%.2f' % savings
+            #binding.pry
+            @total = @total - savings 
+            @total = '%.2f' % savings 
+            "success"
+        end
+    end
+
+end
+
+john = CashRegister.new
+p john.total
 
